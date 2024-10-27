5db62ecf6b87ecc37037dd517d34afe0f625e187
PaganMuffin <50595976+PaganMuffin@users.noreply.github.com>
2024-10-27 13:07:48 +0100

    Add okru support

---
+++ src/scrapper/okru.ts
@@ -0,0 +1,171 @@
+const TEST_METADATA = {
+    playerId: "VideoPopup_player_7495158598273",
+    width: "491",
+    height: "275",
+    notifyEnabled: true,
+    notifyMovieSubscription: true,
+    url: "https://st.okcdn.ru/static/MegaPlayer/10-10-15/vp.swf",
+    url11: "https://st.okcdn.ru/static/MegaPlayer/10-10-15/vp11.swf",
+    html5url: "//st.okcdn.ru/static/MegaPlayer/10-12-24/okHtml5Player.min",
+    okVideoPlayerEnabled: true,
+    minFlashVersionNewPlayer: "11.2",
+    wmode: "opaque",
+    asa: true,
+    provider: "UPLOADED_ODKL",
+    flashvars: {
+        relatedAlways: "1",
+        metadata:
+            '{"provider":"UPLOADED_ODKL","service":"ok","owner":false,"voted":false,"likeCount":0,"subscribed":false,"isWatchLater":false,"slot":0,"siteZone":-1,"showAd":false,"fromTime":0,"author":{},"movie":{"id":"7495158598273","movieId":"7495158598273","likeId":"7495158598273","contentId":"7049249032833","poster":"https://i.okcdn.ru/videoPreview?id=7049249032833\\u0026type=37\\u0026idx=14\\u0026tkn=2kvsSUfKPU05z2FrPb1XouBeTA4\\u0026fn=external_8","duration":"1431","title":"Wybielacz 396_1080p","url":"https://ok.ru/video/7495158598273","link":"/video/7495158598273","collageInfo":{"imageType":"COLLAGE","url":"https://i.okcdn.ru/videoPreview?id=7049249032833\\u0026type=36\\u0026idx=0\\u0026tkn=sBaWTtkGKEP22OXxFYj8FZ0hOrQ","frequency":10,"height":44,"width":80,"count":143,"tileWidth":40,"tileHeight":4},"status":"OK","statusText":"OK","isLive":false,"notPublished":false},"admanMetadata":{},"partnerId":0,"ownerMovieId":"7495158598273","alwaysShowRec":false,"videos":[{"name":"mobile","url":"https://vd282.okcdn.ru/?expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=4\\u0026sig=V5Cevi7BD2Y\\u0026ct=0\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","seekSchema":3,"disallowed":false},{"name":"lowest","url":"https://vd282.okcdn.ru/?expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=0\\u0026sig=EjJtvpHg-FE\\u0026ct=0\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","seekSchema":3,"disallowed":false},{"name":"low","url":"https://vd282.okcdn.ru/?expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=1\\u0026sig=uYvC6vQJ0H0\\u0026ct=0\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","seekSchema":3,"disallowed":false},{"name":"sd","url":"https://vd282.okcdn.ru/?expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=2\\u0026sig=mCDvUzWny3M\\u0026ct=0\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","seekSchema":3,"disallowed":false},{"name":"hd","url":"https://vd282.okcdn.ru/?expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=3\\u0026sig=VPk3Vl0mHio\\u0026ct=0\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","seekSchema":3,"disallowed":false},{"name":"full","url":"https://vd282.okcdn.ru/?expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=5\\u0026sig=YaDOflt-QgM\\u0026ct=0\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","seekSchema":3,"disallowed":false}],"metadataUrl":"https://vd282.okcdn.ru/?expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=1\\u0026sig=uYvC6vQJ0H0\\u0026ct=6\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","hlsManifestUrl":"https://vd282.okcdn.ru/video.m3u8?cmd=videoPlayerCdn\\u0026expires=1730116218072\\u0026srcIp=31.182.251.205\\u0026pr=10\\u0026srcAg=CHROME\\u0026ms=185.226.52.32\\u0026type=2\\u0026sig=mCDvUzWny3M\\u0026ct=8\\u0026urls=45.136.20.42\\u0026clientType=0\\u0026id=7049249032833","failoverHosts":["vd410.okcdn.ru"],"autoplay":{"autoplayEnabled":true,"timeFromEnabled":true,"noRec":false,"fullScreenExit":false,"vitrinaSection":"recommended_movie"},"security":{"url":"https://vd282.okcdn.ru/usr_login","cookie":"vdsig"},"p2pInfo":{"isPeerEnabled":false,"ubsc":0,"pbsc":0,"mptpc":0,"pctmt":0,"pbesc":0,"prrt":0,"srt":0,"swrt":0,"dctt":0},"stunServers":[{"urls":["stun:videostun.mycdn.me:19302"]}]}',
+        saveLastPlayingTimeFrom: "30",
+        castId: "559D7832",
+        noDownload: "1",
+        locale: "en",
+        noChatLikes: "1",
+        noChannel: "1",
+        webmSec: "20",
+        enabledLocalStorage: "1",
+        minCacheTime: "120",
+        maxCachePartOfDurationMQ: "30",
+        checkMQ: "1",
+        noTrailer: "1",
+        recSlot: "7178",
+        noOldDash: "1",
+        noLikeButton: "1",
+        maxCachePartOfDuration: "10",
+        isAnonym: "1",
+        jidx: "1",
+        hideWatermark: "0",
+        isEmbed: "1",
+        noOkliveBanner: "1",
+        minCacheTimeMQ: "20",
+        ldChunk: "2000",
+        showChat: "1",
+        ldBuffer: "6000",
+        feedAdLogic: "15,3,3,14400",
+        siteId: "504",
+        location: "AnonymVideoEmbed/anonymVideoEmbed/anonym",
+        hideExpand: "1",
+        adLogic: "15,0,3,14400",
+        checkAutoplayBrowsers: "safari,chrome,opera,yandex,firefox",
+    },
+    liveRertyTimeout: 5000,
+    poster: "https://i.okcdn.ru/videoPreview?id=7049249032833&type=37&idx=14&tkn=2kvsSUfKPU05z2FrPb1XouBeTA4&fn=external_8",
+    isExternalPlayer: false,
+    isIframePlayer: false,
+    isHtml5Player: true,
+    timestamp: "1730029818079",
+    stubEnabled: false,
+    verifyInline: false,
+    webrtcBrokenH264: false,
+    playerLocalizationEnabled: true,
+};
+
+export class OK {
+    private videoId: string;
+    private metadata: any = {};
+    qualities: { quality: number; url: string }[] = [];
+    title: string = "";
+    thumbnail: string = "";
+
+    constructor(videoId: string) {
+        this.videoId = videoId;
+    }
+
+    public fetchData = async () => {
+        this.metadata = await this.fetchMetadata(this.videoId);
+        console.log(this.metadata.flashvars.metadata);
+        this.qualities = this.extractResolutions(this.metadata);
+    };
+
+    private fetchMetadata = async (videoId: string): Promise<any> => {
+        // return TEST_METADATA;
+        const response = await fetch("https://ok.ru/videoembed/" + videoId, {
+            headers: {
+                accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                Referer:
+                    "https://vk.com/video/@id604627980?z=video604627980_456239207%2Fpl_604627980_-2",
+                "accept-language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+                "cache-control": "max-age=0",
+                "sec-ch-ua":
+                    '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": '"Windows"',
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "none",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1",
+                "user-agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+            },
+            method: "GET",
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch metadata");
+        }
+
+        const respText = await response.text();
+
+        const dataOptions = respText.match(/data-options="([^"]+)"/);
+        if (!dataOptions) {
+            throw new Error("Failed to fetch metadata");
+        }
+
+        const options = dataOptions[1].replace(/&quot;/g, '"');
+        const metadata = JSON.parse(options);
+
+        metadata.flashvars.metadata = JSON.parse(metadata.flashvars.metadata);
+
+        return metadata;
+    };
+
+    private extractResolutions = (
+        metadata: any
+    ): { quality: number; url: string }[] => {
+        const supportedQualities = [
+            {
+                key: "full",
+                quality: 1080,
+            },
+            {
+                key: "hd",
+                quality: 720,
+            },
+            {
+                key: "sd",
+                quality: 480,
+            },
+            {
+                key: "low",
+                quality: 360,
+            },
+            {
+                key: "lowest",
+                quality: 240,
+            },
+            {
+                key: "mobile",
+                quality: 144,
+            },
+        ];
+
+        let resolutions = [];
+
+        for (let quality of supportedQualities) {
+            const video = metadata.flashvars.metadata.videos.find(
+                (video: { name: string; url: string }) =>
+                    video.name === quality.key
+            );
+
+            if (video) {
+                resolutions.push({
+                    quality: quality.quality,
+                    url: video.url,
+                });
+            }
+        }
+
+        return resolutions;
+    };
+}
