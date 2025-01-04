e2d8896052d518147ac33e0754d19e954fb0fb3d
PaganMuffin <50595976+PaganMuffin@users.noreply.github.com>
2025-01-04 21:35:54 +0100

    uncomment middleware with cache

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,28 +6,28 @@ export const onRequest = defineMiddleware(async (context, next) => {
     const { url } = request;
     const { pathname } = new URL(url);
 
-    // console.log("1XD");
-    // if (pathname.startsWith("/player")) {
-    //     console.log("2XD");
-    //     const cacheKay = "http://example.com" + pathname;
-    //     const cache = context.locals.runtime.caches.default;
-    //     const cacheResponse = await cache.match(cacheKay);
-    //     if (cacheResponse) {
-    //         console.log("cached");
-    //         //@ts-ignore
-    //         return new Response(cacheResponse.body, {
-    //             status: cacheResponse.status,
-    //             statusText: cacheResponse.statusText,
-    //             headers: cacheResponse.headers,
-    //         });
-    //     } else {
-    //         console.log("not cached");
-    //         const response = await next();
-    //         response.headers.set("Cache-Control", "public, max-age=60");
-    //         //@ts-ignore
-    //         await cache.put(cacheKay, response.clone());
-    //         return response;
-    //     }
-    // }
+    console.log("1XD");
+    if (pathname.startsWith("/player")) {
+        console.log("2XD");
+        const cacheKay = "http://example.com" + pathname;
+        const cache = context.locals.runtime.caches.default;
+        const cacheResponse = await cache.match(cacheKay);
+        if (cacheResponse) {
+            console.log("cached");
+            //@ts-ignore
+            return new Response(cacheResponse.body, {
+                status: cacheResponse.status,
+                statusText: cacheResponse.statusText,
+                headers: cacheResponse.headers,
+            });
+        } else {
+            console.log("not cached");
+            const response = await next();
+            response.headers.set("Cache-Control", "public, max-age=60");
+            //@ts-ignore
+            await cache.put(cacheKay, response.clone());
+            return response;
+        }
+    }
     return next();
 });
